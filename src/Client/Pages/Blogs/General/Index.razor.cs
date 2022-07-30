ca4cb836945282b82a55bfa098d8e95f78e1689a
James Orson <jamesaorson@gmail.com>
2022-07-30 12:07:46 -0700

    Checkpoint

--- src/Client/Pages/Blogs/General/Index.razor.cs
+++ src/Client/Pages/Blogs/General/Index.razor.cs
@@ -4,58 +4,17 @@ using System.Threading.Tasks;
 using Client.Models.Jorson.Blogs;
 using Client.Services.Jorson;
 
-namespace Client.Pages.Webring.Jorson.Blogs.Food
+namespace Client.Pages.Blogs.General
 {
-	internal class IndexBase : PageBase {}
-
     public partial class Index
     {
-        #region Public
-
-        #region Constructors
-        public Index()
-        {
-            _base = new IndexBase();
-        }
-        #endregion
-
-        #region Constants
-        public static string UserId = "jorson";
-        #endregion
-
-        #endregion
-
-        #region Protected
-
-        #region Member Methods
-        protected override void OnAfterRender(bool firstRender)
-        {
-            base.OnAfterRender(firstRender);
-
-            if (firstRender)
-            {
-                _base.Initialize();
-            }
-        }
-
         protected override async Task OnInitializedAsync()
         {
-            _foodBlogService.UserId = UserId;
-            _blogs = await _foodBlogService.GetAsync("food/food.json");
+            _blogs = await _blogService.GetAsync();
         }
-        #endregion
 
-        #endregion
-
-        #region Private
-
-        #region Members
-        private IEnumerable<FoodBlog> _blogs { get; set; }
+        private IEnumerable<Blog> _blogs { get; set; }
         [Inject]
-        private FoodBlogService _foodBlogService { get; set; }
-        private PageBase _base { get; set; }
-        #endregion
-
-        #endregion
+        private GeneralBlogService _blogService { get; set; }
     }
 }
