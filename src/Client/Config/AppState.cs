e8c90baa259b2f7d88cec620c21428e2b6fc22f2
James Orson <jamesaorson@gmail.com>
2022-07-30 11:24:54 -0700

    Remove regions

--- src/Client/Config/AppState.cs
+++ src/Client/Config/AppState.cs
@@ -4,14 +4,8 @@ namespace Client.Config
 {
     public static class AppState
     {
-        #region Public
-
-        #region Defaults
         public const string DEFAULT_FAVICON_URI = "favicons/favicon.ico";
-        public const bool DEFAULT_IS_SIDENAV_HIDDEN = false;
-        #endregion
 
-        #region Fields
         public static string FaviconUri
         {
             get => _faviconUri;
@@ -24,28 +18,14 @@ namespace Client.Config
         }
         public static event Action OnFaviconUriChange;
         public static event Action OnChange;
-        #endregion
 
-        #region Methods
         public static void Reset()
         {
             FaviconUri = DEFAULT_FAVICON_URI;
         }
-        #endregion
 
-        #endregion
-
-        #region Private
-
-        #region Fields
         private static string _faviconUri { get; set; } = DEFAULT_FAVICON_URI;
-        private static bool _isSideNavHidden { get; set; } = DEFAULT_IS_SIDENAV_HIDDEN;
-        #endregion
 
-        #region Methods
         private static void NotifyStateChanged() => OnChange?.Invoke();
-        #endregion
-
-        #endregion
     }
 }
