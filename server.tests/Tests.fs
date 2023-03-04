7efb1af3ebf646443d4d8f02bbf62698717701d9
James Orson <jamesaorson@gmail.com>
2023-03-04 11:14:21 -0700

    Update Tests.fs

--- server.tests/Tests.fs
+++ server.tests/Tests.fs
@@ -5,18 +5,26 @@ open TestApi
 open Xunit
 
 [<Fact>]
-let ``/ should return "Hello world"`` () =
+let ``/api/v1 should return "Hello world"`` () =
   task {
     let api = runTestApi().CreateClient()
-    let! response = api.GetAsync("/")
+    let! response = api.GetAsync("/api/v1")
     let! responseContent = response.Content.ReadAsStringAsync()
     Assert.Equal("Hello world", responseContent)
   }
 
-let ``/ping should return "pong"`` () =
+let ``/api/v1/ should return "Hello world"`` () =
   task {
     let api = runTestApi().CreateClient()
-    let! response = api.GetAsync("/ping")
+    let! response = api.GetAsync("/api/v1/")
+    let! responseContent = response.Content.ReadAsStringAsync()
+    Assert.Equal("Hello world", responseContent)
+  }
+
+let ``/api/v1/ping should return "pong"`` () =
+  task {
+    let api = runTestApi().CreateClient()
+    let! response = api.GetAsync("/api/v1/ping")
     let! responseContent = response.Content.ReadAsStringAsync()
     Assert.Equal("pong", responseContent)
   }
