bd5a57abca2dacc5484aeabea60456dba156f8cc
Brandon Apol <brandonapol@cedarville.edu>
2023-03-09 21:29:43 -0500

    cleared excess

--- client/src/components/__tests__/HelloWorld.spec.ts
+++ client/src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,13 @@
-import { describe, it, expect } from 'vitest'
+// import { describe, it, expect } from 'vitest'
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+// import { mount } from '@vue/test-utils'
+// import HelloWorld from '../HelloWorld.vue'
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+// describe('HelloWorld', () => {
+//   it('renders properly', () => {
+//     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+//     expect(wrapper.text()).toContain('Hello Vitest')
+//   })
+// })
+// TODO - why are we using vitest AND cypress?
+export {}
