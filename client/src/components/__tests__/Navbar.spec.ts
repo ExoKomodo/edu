ae65c2477a83b1e2e294827e0934a413e153b918
Brandon Apol <brandonapol@cedarville.edu>
2023-03-09 22:47:11 -0500

    added test for navbar

--- client/src/components/__tests__/Navbar.spec.ts
+++ client/src/components/__tests__/Navbar.spec.ts
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Navbar from '../Navbar.vue'
 
-describe('HelloWorld', () => {
+describe('Navbar', () => {
   it('renders properly', () => {
     const wrapper = mount(Navbar)
     expect(wrapper.text()).toContain('Edu')
