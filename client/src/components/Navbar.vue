e990d2980e84c2458b5b9a15b935b8a03ec92ff9
James Orson <jamesaorson@gmail.com>
2023-03-10 22:10:27 -0800

    Fix links in navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,34 +1,53 @@
 <template>
-    <nav class="bg-richBlack">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16">
-          <div class="flex-shrink-0">
-            <a href="#" class="flex items-center">
-              <span class="ml-2 p-2 rounded text-white text-xl transition duration-250 hover:text-white hover:bg-midnightGreen font-bold">
-                <span class="text-tiffanyBlue">EK</span>.<strong>Edu</strong>
-              </span>
+  <nav class="bg-richBlack">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex-shrink-0">
+          <a href="#"
+             class="flex items-center">
+            <span class="ml-2 p-2 rounded text-white text-xl transition duration-250 hover:text-white hover:bg-midnightGreen font-bold">
+              <span class="text-tiffanyBlue">EK</span>.<strong>Edu</strong>
+            </span>
+          </a>
+        </div>
+        <div class="hidden md:block">
+          <div class="ml-4 flex items-center md:ml-6">
+            <!-- TODO - fix hidden items, swap with hamburger menu & dropdown -->
+            <a v-for="link in links"
+               :href=link.href
+               class="px-3 py-2 transition duration-250 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-midnightGreen bg-gray-800">
+              {{ link.text }}
             </a>
-          </div>
-          <div class="hidden md:block">
-            <div class="ml-4 flex items-center md:ml-6">
-                <!-- TODO - write a loop and highlight active page; add Link instead -->
-                <!-- TODO - fix hidden items, swap with hamburger menu & dropdown -->
-              <a href="#" class="px-3 py-2 transition duration-250 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-midnightGreen bg-gray-800">Home</a>
-              <a href="#" class="ml-4 px-3 transition duration-250 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-midnightGreen">About</a>
-              <a href="#" class="ml-4 px-3 transition duration-250 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-midnightGreen">Contact</a>
-            </div>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
 </template>
   
 <script lang="ts">
-import Link from 'vue-router'
 import { defineComponent } from 'vue';
-// TODO - update a tags to links
+
 export default defineComponent({
   name: 'Navbar',
+  data() {
+    return {
+      links: [
+        {
+          text: 'Home',
+          href: '/',
+        },
+        {
+          text: 'About',
+          href: '/about',
+        },
+        {
+          text: 'Contact',
+          href: '/contact',
+        },
+      ]
+    };
+  }
 });
 </script>
 
---
+++ client/src/views/ContactView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contact">
+    <h1>Don't</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .contact {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
