0240bb3446a08d5aa0ed4ca8590ff86711e697ab
James Orson <jamesaorson@gmail.com>
2023-03-27 16:58:27 +0000

    Fix navbar test

--- client/src/components/__tests__/Navbar.spec.ts
+++ client/src/components/__tests__/Navbar.spec.ts
@@ -1,12 +1,12 @@
 import { describe, it, expect } from 'vitest'
 
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Navbar from '../Navbar.vue'
 
 describe('Navbar', () => {
   it('renders properly', () => {
-    const wrapper = mount(Navbar)
-    expect(wrapper.text()).toContain('Edu')
+    const wrapper = shallowMount(Navbar);
+    expect(wrapper.text()).toContain('Edu');
   })
 })
 
