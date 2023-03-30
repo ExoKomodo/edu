6d8b2be58dca23cc62bef1ce4ab67c64c32c1d73
Brandon Apol <brandonapol@cedarville.edu>
2023-03-30 14:39:08 -0400

    update lorem content

--- client/src/components/InfoTile.vue
+++ client/src/components/InfoTile.vue
@@ -2,10 +2,9 @@
     <div class="relative rounded-lg my-10 p-6 m-5 bg-opacity-50 backdrop-filter backdrop-blur-lg bg-white shadow-xl xs:flex xs:flex-row sm:flex-col flex-col">
         <p class="text-2xl font-bold">{{ props.header }}</p>
         <p class="xs:hidden sm:block">
-            Lorem ipsum dolor sit, amet consectetur 
-            adipisicing elit. Numquam ex blanditiis at, qui deleniti, nostrum 
-            nihil autem vel sequi voluptatum, fuga voluptate reiciendis! Ullam 
-            ipsum corporis eveniet possimus quam ex.
+            Learn more about coding with our highly qualified instructors. <a href="#">EK.edu</a> is oriented around
+            bootcamp graduates, CS graduates, and others who are interested in becoming artisans of software. We aim to 
+            take learners beyond the basics of how to write code, and focus on transforming into an artist of programming. 
         </p>
     </div>
 </template>
