25a65bc55094cda8c2844c7ae72d23e60ad089e4
Brandon Apol <72466214+brandonapol@users.noreply.github.com>
2023-04-01 15:38:15 -0400

    42: Update instructor bios

--- client/src/components/Instructors.vue
+++ client/src/components/Instructors.vue
@@ -5,25 +5,23 @@
                 <img src="../assets/images/james.jpg" alt="James Pic" class="rounded w-[40%]"/>
                 <div class="ml-4 xs:my-3 ">
                     <p class="text-virgil">James Orson</p>
-                    <p class="text-virgil xs:hidden sm:block">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Consectetur illum ducimus, deserunt ad, ab a quaerat aspernatur
-                        dolore perferendis accusamus vel iure placeat distinctio odit
-                        quisquam voluptatibus eaque provident, voluptates et numquam
-                        iusto. Ratione, perspiciatis quis doloremque recusandae sit
-                        consequatur eveniet minima laudantium repudiandae placeat
-                        natus suscipit qui necessitatibus iste.</p>
+                    <p class="text-virgil xs:hidden sm:block">James is a chief Site Reliability Engineer at Pure Storage  
+                        in Mountain View, California. He and his wife love their daughter, eating healthy, and participating 
+                        in their local church life. James has written his own programming language, Daybreak, which you can 
+                        look at and learn about <a href="https://github.com/ExoKomodo/daybreak">here.</a>
+                    </p>
                 </div>
             </div>
             <div class="xs:flex xs:flex-col-reverse md:flex-row rounded-lg my-10 p-6 m-5 bg-opacity-50 backdrop-filter backdrop-blur-lg bg-white shadow-xl flex items-center">
                 <div class="ml-4 xs:my-3">
                     <p class="text-virgil">Brandon Apol</p>
-                    <p class="text-virgil xs:hidden sm:block">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Consectetur illum ducimus, deserunt ad, ab a quaerat aspernatur
-                        dolore perferendis accusamus vel iure placeat distinctio odit
-                        quisquam voluptatibus eaque provident, voluptates et numquam
-                        iusto. Ratione, perspiciatis quis doloremque recusandae sit
-                        consequatur eveniet minima laudantium repudiandae placeat
-                        natus suscipit qui necessitatibus iste.</p>
+                    <p class="text-virgil xs:hidden sm:block">
+                        Brandon is a former bootcamp instructor who now works with a government contractor. He loves 
+                        teaching and taught music in Alaskan elementary schools for five years before moving into tech. 
+                        Brandon loves the outdoors, particularly rock climbing, and currently resides in central Ohio. 
+                        His portfolio website and contact info can be found 
+                        <a href="https://personal-flask-app-do-5qgwm.ondigitalocean.app/">here</a>.
+                    </p>
                 </div>
                 <img src="../assets/images/brandon.jpg" alt="Brandon Pic" class="rounded w-[40%]"/>
                 
