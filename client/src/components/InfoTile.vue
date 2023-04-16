f9eb0ef6acc98bafa6fb440b0394a6e5e037f04e
Brandon Apol <brandonapol@cedarville.edu>
2023-04-15 20:13:26 -0400

    fixed hidden merge conflict

--- client/src/components/InfoTile.vue
+++ client/src/components/InfoTile.vue
@@ -2,15 +2,9 @@
     <div class="relative rounded-lg my-10 p-6 m-5 bg-opacity-50 backdrop-filter backdrop-blur-lg bg-white shadow-xl xs:flex xs:flex-row sm:flex-col flex-col">
         <p class="text-2xl font-bold">{{ props.header }}</p>
         <p class="xs:hidden sm:block">
-<<<<<<< HEAD
-            Learn more about coding with our highly qualified instructors. <a href="#">EK.edu</a> is oriented around
-            bootcamp graduates, CS graduates, and others who are interested in becoming artisans of software. We aim to 
-            take learners beyond the basics of how to write code, and focus on transforming into an artist of programming. 
-=======
             Learn more about coding with our highly qualified instructors. <a href="/">EK.edu</a> is oriented around
             bootcamp graduates, CS graduates, and others who are interested in becoming artisans of software. We aim to 
             take learners beyond the basics of how to write code, and focus on transforming into an artist of programming.
->>>>>>> 76a7a98b8e2420eeab224d65d09709a192163566
         </p>
     </div>
 </template>
