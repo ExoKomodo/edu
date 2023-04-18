8727e3e1ea9f7e42bf4d71654bc693324d5dfee9
Brandon Apol <72466214+brandonapol@users.noreply.github.com>
2023-04-18 09:07:11 -0400

    35: Fix unreadable blog styling

--- client/src/components/InfoTile.vue
+++ client/src/components/InfoTile.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="relative rounded-lg my-10 p-6 m-5 bg-opacity-50 backdrop-filter backdrop-blur-lg bg-white shadow-xl xs:flex xs:flex-row sm:flex-col flex-col">
         <p class="text-2xl font-bold">{{ props.header }}</p>
-        <p class="xs:hidden sm:block">
-            Learn more about coding with our highly qualified instructors. <a href="/">EK.edu</a> is oriented around
-            bootcamp graduates, CS graduates, and others who are interested in becoming artisans of software. We aim to 
-            take learners beyond the basics of how to write code, and focus on transforming into an artist of programming.
+        <p class="xs:hidden sm:block" v-html="instructorsDescription">
         </p>
     </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps(['header'])
+
+const instructorsDescription = `Learn more about coding with our highly qualified instructors. <a href="/">EK.edu</a> is oriented around
+            bootcamp graduates, CS graduates, and others who are interested in becoming artisans of software. We aim to 
+            take learners beyond the basics of how to write code, and focus on transforming into an artist of programming.`
 </script>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-richBlack shadow-lg">
+  <nav class="bg-richBlack shadow-lg sticky">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between h-[8vh]">
         <div class="flex-shrink-0">
@@ -11,7 +11,7 @@
             </span>
           </a>
         </div>
-        <div class="hidden md:block">
+        <div v-if="!isSmall">
           <div class="ml-4 flex items-center md:ml-6">
             <!-- TODO - fix hidden items, swap with hamburger menu & dropdown -->
             <RouterLink v-for="route in routes"
@@ -25,14 +25,52 @@
             </RouterLink>
           </div>
         </div>
+        <div  v-else>
+          <div>
+            <div
+              @click="handleClick"
+              v-if="!visible"
+              class="text-white border-2 border-slate-500 p-2 rounded">
+              <i class="fas fa-bars"></i>
+            </div>
+            <div
+              v-else
+              @click="handleClick"
+              class="text-white border-2 border-slate-500 p-2 rounded">
+              <i class="fas fa-times"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-show="visible" >
+      <div class="flex items-center flex-row justify-evenly navBorder">
+        <RouterLink v-for="route in routes"
+            :to=route.path
+            class="px-3 m-2 py-2 transition duration-250 rounded-md text-sm 
+            font-medium text-gray-300 hover:text-virgil hover:bg-midnightGreen
+             bg-gray-800"
+            >
+            <div>
+              {{ route.name }}
+            </div>
+        </RouterLink>
       </div>
     </div>
   </nav>
 </template>
   
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router';
 import router from '../router/index';
+const isSmall = window.innerWidth <= 640
+
+const visible = ref(false);
+const handleClick = () => {
+  console.log(`Value: ${visible.value}`)
+  visible.value = !visible.value
+}
 
 const routes = router.options.routes.filter(route => !route.props);
 </script>
