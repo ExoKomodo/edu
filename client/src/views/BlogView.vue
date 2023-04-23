01c03aedf28fae947e05d5da5f4d9645002e27a1
JamesOrson <jamesaorson@gmail.com>
2023-04-23 02:28:46 -0700

    Use new colors

--- client/src/views/BlogView.vue
+++ client/src/views/BlogView.vue
@@ -19,9 +19,9 @@ function castToBlogMetadata(value: [string, BlogMetadata]) {
 <template>
   <div class="blogBackground min-h-screen">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 my-5">
-      <h1 class="p-2 bg-gray-800 text-white rounded flex justify-center text-3xl font-bold my-3">blogs</h1>
+      <h1 class="p-2 bg-mysticStone text-white rounded flex justify-center text-3xl font-bold my-3">blogs</h1>
         <BlogLink v-for="(blog, id) of blogIndex.blogs"
-                  class="p-2 bg-gray-800 text-white rounded flex pl-5 my-3"
+                  class="p-2 bg-mysticStone text-white rounded flex pl-5 my-3"
                   :id="castToBlogId(id)"
                   :title="castToBlogMetadata(blog).title"
                   :description=" castToBlogMetadata(blog).description" />
