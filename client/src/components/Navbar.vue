071b6ad713fa4c2ab4baad080154bb981a3a3f6b
JamesOrson <jamesaorson@gmail.com>
2023-04-23 02:29:25 -0700

    Set up new bug route, resize navbar elements, and refactor setup code

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -16,20 +16,30 @@
             <!-- TODO - fix hidden items, swap with hamburger menu & dropdown -->
             <RouterLink v-for="route in routes"
                 :to=route.path
-                class="px-3 m-2 py-2 transition duration-250 rounded-md text-sm 
-                font-medium text-gray-300 hover:text-virgil hover:bg-midnightGreen bg-gray-800 xs:hidden sm:block"
+                class="px-3 m-2 py-1 transition duration-250 rounded-md text-sm 
+                font-medium text-gray-300 hover:text-virgil hover:bg-midnightGreen bg-mysticStone xs:hidden sm:block"
                 >
                 <div>
                   {{ route.name }}
                 </div>
             </RouterLink>
+            <hr class="bg-virgil color-virgil w-3 border-1.5"/>
+            <a
+              :href=bugRoute.path
+              class="px-3 m-2 py-1 transition duration-250 rounded-md text-sm 
+                font-medium text-gray-300 hover:text-virgil hover:bg-rust bg-rufous xs:hidden sm:block"
+              target="_blank">
+                <div>
+                  {{ bugRoute.name }}
+                </div>
+            </a>
           </div>
         </div>
         <div  v-else>
           <div>
             <div
               @click="handleClick"
-              v-if="!visible"
+              v-if="!isVisible"
               class="text-white border-2 border-slate-500 p-2 rounded">
               <i class="fas fa-bars"></i>
             </div>
@@ -43,18 +53,33 @@
         </div>
       </div>
     </div>
-    <div v-show="visible" >
+    <div v-show="isVisible" >
       <div class="flex items-center flex-row justify-evenly navBorder">
         <RouterLink v-for="route in routes"
             :to=route.path
-            class="px-3 m-2 py-2 transition duration-250 rounded-md text-sm 
+            class="px-2 m-1 py-1.5 transition duration-250 rounded-md text-sm 
             font-medium text-gray-300 hover:text-virgil hover:bg-midnightGreen
-             bg-gray-800"
+             bg-mysticStone"
             >
             <div>
               {{ route.name }}
             </div>
         </RouterLink>
+        <!-- Needs the wrapping span to obey flex rules -->
+        <span>
+          <hr class="bg-virgil color-virgil w-3  border-1.5"/>
+        </span>
+        <a
+          :href=bugRoute.path
+          class="px-2 m-1 py-1.5 transition duration-250 rounded-md text-sm 
+            font-medium text-gray-300 hover:text-virgil hover:bg-rust
+             bg-rufous"
+          target= '_blank'
+          >
+            <div>
+              {{ bugRoute.name }}
+            </div>
+        </a>
       </div>
     </div>
   </nav>
@@ -64,13 +89,24 @@
 import { ref } from 'vue'
 import { RouterLink } from 'vue-router';
 import router from '../router/index';
-const isSmall = window.innerWidth <= 640
 
-const visible = ref(false);
+const routes = router.options.routes.filter(
+  route => !route.props
+);
+const bugRoute = {
+  name: 'bug?',
+  path: 'https://github.com/ExoKomodo/Edu/issues/new/choose',
+};
+
+const isVisible = ref(false);
 const handleClick = () => {
-  console.log(`Value: ${visible.value}`)
-  visible.value = !visible.value
+  isVisible.value = !isVisible.value;
 }
 
-const routes = router.options.routes.filter(route => !route.props);
+const isSmall = ref(false);
+function updateWindowSize() {
+  isSmall.value = window.innerWidth <= 640;
+}
+window.addEventListener("resize", updateWindowSize);
+updateWindowSize();
 </script>
