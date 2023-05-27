e3407278f1ef289740d5aef2c1409b56ccd7ef56
James Orson <jamesaorson@gmail.com>
2023-05-26 21:20:00 -0700

    133: Remove bug button

--- client/src/components/Button.vue
+++ client/src/components/Button.vue
@@ -2,7 +2,7 @@
   <a
     @click="handler"
     class="px-3 m-2 py-1 transition duration-250 rounded-md text-sm
-    font-medium text-gray-300 hover:text-virgil hover:bg-midnightGreen bg-mysticStone xs:hidden sm:block">
+    font-medium text-gray-300 hover:text-virgil hover:bg-midnightGreen bg-mysticStone sm:block">
     <div>
       {{ text }}
     </div>
