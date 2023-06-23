01a87df923bc678588616a039b418c5a203d9545
James Orson <jamesaorson@gmail.com>
2023-06-23 12:40:06 -0700

    Fix ordering

--- client/src/components/CourseEditor.vue
+++ client/src/components/CourseEditor.vue
@@ -1,39 +1,47 @@
 <template>
-  <div v-if="AuthService.isAdmin(auth0)" class="bg-mysticStone text-white border-slate-400 border-2 rounded p-1 pl-2 my-2">
+  <div v-if="AuthService.isAdmin(auth0)"
+       class="bg-mysticStone text-white border-slate-400 border-2 rounded p-1 pl-2 my-2">
     <div class="text-2xl">Course Editor</div>
     <input type="checkbox"
-          id="edit-mode"
-          name="editMode"
-          v-model="state.isEditMode">
+           id="edit-mode"
+           name="editMode"
+           v-model="state.isEditMode">
     <label for="edit-mode"> {{ handlerText }} mode?</label>
-    <span class="inline-block" :class="{ invisible: !state.isEditMode }">
+    <span class="inline-block"
+          :class="{ invisible: !state.isEditMode }">
       <Button :handler="() => handler(state)"
               :text="handlerText"></Button>
     </span>
     <div :class="{ invisible: !state.isEditMode }">
       <input type="checkbox"
-            id="show-preview"
-            @change="onPreviewChangeAsync"
-            name="showPreview"
-            v-model="state.showPreview">
+             id="show-preview"
+             @change="onPreviewChangeAsync"
+             name="showPreview"
+             v-model="state.showPreview">
       <label for="show-preview"> Show preview?</label>
     </div>
     <div v-if="AuthService.isAdmin(auth0) && state.isEditMode">
-      <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-12">Id</div>
-      <CodeEditor v-model="state.id" :height="2"></CodeEditor>
       <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-12">Name</div>
-      <CodeEditor v-model="state.name" :height="2"></CodeEditor>
-      <p v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview" class="text-2xl font-bold border-slate-400 rounded border-2 p-1 pl-2">{{ state.name?.toUpperCase() }}</p>
+      <CodeEditor v-model="state.name"
+                  :height="2"></CodeEditor>
+      <p v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview"
+         class="text-2xl font-bold border-slate-400 rounded border-2 p-1 pl-2">{{ state.name?.toUpperCase() }}</p>
       <div class="text-white border-white border-2 rounded p-1 pl-2 p my-2 mr-1">Description</div>
       <CodeEditor v-model="state.description"
                   :height="2"></CodeEditor>
-      <p v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview" class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2">{{ state.description }}</p>
+      <p v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview"
+         class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2">{{ state.description }}</p>
       <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-8">Content</div>
       <CodeEditor v-model="state.content"
                   language="html"
                   v-on:update:model-value="onContentChangeAsync"
                   :height="40"></CodeEditor>
-      <div v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview" class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2" v-html="state.templatedContent"></div>
+      <div v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview"
+           class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2"
+           v-html="state.templatedContent"></div>
+      <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-12">Id</div>
+      <CodeEditor v-model="state.id"
+                  :height="2"></CodeEditor>
     </div>
   </div>
 </template>
