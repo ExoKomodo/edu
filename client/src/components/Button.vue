c9e4343e6875d9d778fe2fc35bc98e56fd9cb608
Brandon Apol <brandonapol@cedarville.edu>
2023-06-24 12:11:06 -0400

    replace a tag with button tag for hover consistency

--- client/src/components/Button.vue
+++ client/src/components/Button.vue
@@ -1,12 +1,12 @@
 <template>
-  <a
+  <button
     @click="handler"
     class="px-3 m-2 py-1 transition duration-250 rounded-md text-sm
     font-medium text-gray-300 hover:text-virgil hover:bg-midnightGreen bg-mysticStone sm:block">
     <div>
       {{ text }}
     </div>
-  </a>
+  </button>
 </template>
 
 <script setup lang="ts">
