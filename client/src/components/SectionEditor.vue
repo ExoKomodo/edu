65fcb2063d7705572b2c3cc3cd01305289b24096
James Orson <jamesaorson@gmail.com>
2023-07-03 06:57:14 -0700

    155: Add demo course

--- client/src/components/SectionEditor.vue
+++ client/src/components/SectionEditor.vue
@@ -1,28 +1,30 @@
 <template>
-  <div v-if="AuthService.isAdmin(auth0)" class="bg-mysticStone text-white border-slate-400 border-2 rounded p-1 pl-2 my-2">
+  <div v-if="AuthService.isAdmin(auth0)"
+       class="bg-mysticStone text-white border-slate-400 border-2 rounded p-1 pl-2 my-2">
     <div class="text-2xl">Section Editor</div>
     <input type="checkbox"
-          id="edit-mode"
-          name="editMode"
-          v-model="state.isEditMode">
+           id="edit-mode"
+           name="editMode"
+           v-model="state.isEditMode">
     <label for="edit-mode"> {{ handlerText }} mode?</label>
-    <span class="inline-block" :class="{ invisible: !state.isEditMode }">
+    <span class="inline-block"
+          :class="{ invisible: !state.isEditMode }">
       <Button :handler="() => handler(state)"
               :text="handlerText"></Button>
     </span>
     <div :class="{ invisible: !state.isEditMode }">
       <input type="checkbox"
-            id="show-preview"
-            name="showPreview"
-            v-model="state.showPreview">
+             id="show-preview"
+             name="showPreview"
+             v-model="state.showPreview">
       <label for="show-preview"> Show preview?</label>
     </div>
     <div v-if="AuthService.isAdmin(auth0) && state.isEditMode">
-      <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-12">Id</div>
-      <CodeEditor v-model="state.id" :height="2"></CodeEditor>
       <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-12">Name</div>
-      <CodeEditor v-model="state.name" :height="2"></CodeEditor>
-      <p v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview" class="text-2xl font-bold border-slate-400 rounded border-2 p-1 pl-2">{{ state.name?.toUpperCase() }}</p>
+      <CodeEditor v-model="state.name"
+                  :height="2"></CodeEditor>
+      <p v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview"
+         class="text-2xl font-bold border-slate-400 rounded border-2 p-1 pl-2">{{ state.name?.toUpperCase() }}</p>
       <div class="text-white border-white border-2 rounded p-1 pl-2 p my-2 mr-1">Description</div>
       <CodeEditor v-model="state.description"
                   :height="2"></CodeEditor>
@@ -35,7 +37,18 @@
       <CodeEditor v-model="state.content"
                   language="html"
                   :height="40"></CodeEditor>
-      <div v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview" class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2" v-html="state.content"></div>
+      <div v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview"
+           class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2"
+           v-html="state.content"></div>
+      <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-8">Difficulty (integer)</div>
+      <CodeEditor v-model="state.difficulty"
+                  :height="2"></CodeEditor>
+      <div v-if="AuthService.isAdmin(auth0) && state.isEditMode && state.showPreview"
+           class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2"
+           v-html="state.difficulty"></div>
+      <div class="text-white border-white border-2 rounded p-1 pl-2 my-2 mr-12">Id</div>
+      <CodeEditor v-model="state.id"
+                  :height="2"></CodeEditor>
     </div>
   </div>
 </template>
--- client/src/services/BlogService.ts
+++ client/src/services/BlogService.ts
@@ -7,7 +7,6 @@ export default class BlogService {
       return await HttpServiceV1.getAsync<Blog>('blog', id, options);
     }
     catch (err: any) {
-      console.error(err);
       options.toast?.error(`Failed to get blog: ${err}`);
       throw err;
     }
