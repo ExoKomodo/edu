1293d39bd38d7d73352a6175f0550128a0ecf768
James Orson <jamesaorson@gmail.com>
2024-04-12 16:20:38 -0700

    Fix AuthService types

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -1,13 +1,14 @@
 import HttpServiceV1, { type HttpOptions } from './HttpServiceV1';
-import type { Auth0VueClient } from '@auth0/auth0-vue';
+import type { Auth0VueClient, User } from '@auth0/auth0-vue';
 import type { UserInfo } from '@/models';
+import type { Ref } from 'vue';
 
 export default class AuthService {
-  static async getAccessTokenAsync(auth0: Auth0VueClient, options: HttpOptions={}): Promise<string | null> {
+  static async getAccessTokenAsync(auth0: Auth0VueClient, options: HttpOptions = {}): Promise<string | null> {
     return auth0.getAccessTokenSilently();
   }
 
-  static async getUserInfoAsync(auth0: Auth0VueClient, options: HttpOptions={}): Promise<UserInfo> {
+  static async getUserInfoAsync(auth0: Auth0VueClient, options: HttpOptions = {}): Promise<UserInfo> {
     try {
       return await HttpServiceV1.getAsync<UserInfo>(
         'user',
@@ -23,10 +24,14 @@ export default class AuthService {
   }
 
   static isAdmin(auth0: Auth0VueClient): boolean {
-    return auth0.isAuthenticated && [
+    if (auth0.user.value === null || !auth0.isAuthenticated) {
+      return false;
+    }
+    const user: Ref<User> = auth0.user as Ref<User>;
+    return [
       "exokomodo@gmail.com",
       "brandonapol@cedarville.edu",
-    ].includes(auth0.user.value.email ?? '');
+    ].includes(user.value.email ?? '');
   }
 
   static login(auth0: Auth0VueClient): void {
