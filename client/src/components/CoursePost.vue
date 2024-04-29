d664176d0b9ce0ebf9d0b0488228d402c8ecccb1
James Orson <jamesaorson@gmail.com>
2024-04-29 07:47:21 -0700

    Fix coursepost

--- client/src/components/CoursePost.vue
+++ client/src/components/CoursePost.vue
@@ -7,8 +7,8 @@
         v-html="state.description"></p>
       <div v-if="auth0.isAuthenticated" class="text-xl border-slate-400 rounded border-2 p-1 pl-2 my-2"
         v-html="props.templatedContent"></div>
-      <!-- <CourseEditor :handler="saveCourseAsync" handlerText="Update" :courseId="state.id" :courseContent="state.content"
-        :courseDescription="state.description" :courseName="state.name"></CourseEditor> -->
+      <CourseEditor :handler="saveCourseAsync" handlerText="Update" :courseId="state.id" :courseContent="state.content"
+        :courseDescription="state.description" :courseName="state.name"></CourseEditor>
     </div>
   </div>
 </template>
@@ -16,6 +16,7 @@
 <script setup lang="ts">
 import AuthService from '@/services/AuthService';
 import CourseService from '@/services/CourseService';
+import CourseEditor from '@/components/CourseEditor.vue';
 import type { Course, CourseEditorState } from '@/models';
 import { reactive } from 'vue';
 import { useAuth0 } from '@auth0/auth0-vue';
